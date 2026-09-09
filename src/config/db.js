534afc275499f980fce902eec3bad38a4e83a763
ganssoftware <ganssoftware1@gmail.com>
2026-09-10 00:36:49 +0700

    second commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,13 @@
 const { Pool } = require("pg");
+
 require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
 });
 
 pool.on("connect", () => {
